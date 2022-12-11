a3827d947328b1114b9e84a6b0f447a3145616ff
Ali Karabulut <a.kbt6666@gmail.com>
2022-12-11 20:14:31 +0300

    Redux temel kavramlar 2

--- src/redux-temel-kavramlar.js
+++ src/redux-temel-kavramlar.js
@@ -1,22 +1,31 @@
-const redux = require('redux') //bu şekilde içe aktarılıyor
+const redux = require("redux"); //bu şekilde içe aktarılıyor
 
-const counterReducer = (state = {counter: 0}, action) => { 
-
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
     return {
-        counter: state.counter +1
-    }
-}
+      counter: state.counter + 1,
+    };
+  }
 
-const store = redux.createStore(counterReducer) //Store yaratır
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer); //Store yaratır
 
 // console.log(store.getState())
 
 const counterSubscriber = () => {
+  const latestState = store.getState(); // güncellendikten sora en son state snapshot ını
+  console.log(latestState);
+};
 
-    const latestState = store.getState() // güncellendikten sora en son state snapshot ını 
-    console.log(latestState)
-}
+store.subscribe(counterSubscriber);
 
-store.subscribe(counterSubscriber)
-
-store.dispatch({ type: 'increment'})
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
